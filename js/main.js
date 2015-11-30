7df81192561cc2ffc07df8b95b297311a174acc4
Jason Demetri <jsd219@gmail.com>
2015-11-30 11:33:13 -0600

    init

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+$(function(){
+	var todoItems = new TodoItems();
+	todoItems.fetch();
+
+	var todoItemsView = new TodoItemsView({ model: todoItems });
+	$("body").append(todoItemsView.render().$el);
+});
